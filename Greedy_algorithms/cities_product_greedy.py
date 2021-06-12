164fd760fa89a2807a5c3a2c5b6119032f965adc
lukas <lukasz.kordon@outlook.com>
2021-06-12 23:23:46 +0200

    add greedty alghoritm and modyfie DFS

---
+++ Greedy_algorithms/cities_product_greedy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 12 20:33:09 2021
+- set cover problem
+- where should I go to buy all products and visit little places?
+@author: lukas
+"""
+
+# products I want to buy
+cities_list = set(["bread", "sugar", "butter", "meat", "rice", "tomato",
+                                                       "oil", "yoghurt"])
+
+# wher are those products?
+products = {}
+products["kl"] = set(["bread", "butter", "rice", "oil", "yoghurt"])
+products["po"] = set(["sugar",  "tomato", "oil"])
+products["wr"] = set(["bread", "sugar", "butter", "rice", "yoghurt"])
+products["gd"] = set(["meat", "rice", "oil"])
+products["wa"] = set(["sugar", "rice", "tomato", "yoghurt"])
+
+# our final destination plan
+final_destinations = set()
+
+while cities_list:
+    # best place to start shoping (a lot of products there)
+    best_desti = None
+    # new products (not in previous cities)
+    new_produc = set()
+    for product, cities in products.items():
+        # set intersection
+        covered = cities_list & cities
+        if len(covered) > len(new_produc):
+            best_desti = product
+            new_produc = covered
+            
+    cities_list -= new_produc
+    final_destinations.add(best_desti)
+
+print(final_destinations)
+
+        
+        
+    
