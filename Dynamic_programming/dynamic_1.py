942aff8f0f06910d828d5039d27eef404a75b439
lukas <lukasz.kordon@outlook.com>
2021-06-17 22:12:21 +0200

    new folder

---
+++ Dynamic_programming/dynamic_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 21:14:30 2021
+- when we can divide main problem into subproblems
+- similar to recursion but not identical
+- like inducting reasoning in practice
+@author: lukas
+"""
+
+products = {}
+products["water"] = 
+products["book"] =
+products["food"] =
+products["jacket"] =
+products["camera"] =
